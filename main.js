6f1c044d72df1587bed9349e6b7200dbb67f6b77
Rudy <rudy311013@gmail.com>
2023-01-20 14:42:42 -0500

    commit

---
+++ main.js
@@ -0,0 +1,6 @@
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.row.row-cols-1.row-cols-sm-2.row-cols-md-4.g-2', {delay:200});
+ScrollReveal().reveal('.news-cards', {delay:500});
+ScrollReveal().reveal('.cards-banner-one', {delay:500});
+ScrollReveal().reveal('.cards-banner-two', {delay:500});
